c74fce20aa4bb62a73f070c6a2750052accf3968
s.yamada <syamada@netprotections.co.jp>
2022-09-10 11:28:19 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,28 @@
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
+    {{ $store.state.count }}
+    <br>
+    <ul>
+      <li v-for="user in visibleUsers" :key="user.id"> 
+      {{ user.id }} : {{ user.name }}: {{ user.isVisible }}
+      </li>
+    </ul>
+    {{ getUserById.name }}
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  
+  export default {
+    computed: {
+      visibleUsers() {
+        return this.$store.getters.visibleUsers;
+      },
+      getUserById() {
+        return this.$store.getters.getUserById(2);
+      }
+    }
+  }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,38 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state: { // 初期値
+    count: 0,
+    users: [
+      {id: 1, name: '大谷', isVisible: true },
+      {id: 2, name: 'ダルビッシュ', isVisible: false },
+      {id: 3, name: '錦織', isVisible: true },
+    ]
   },
   getters: {
+    // visibleUsers(state) {
+    //   return state.users.filter( user => user.isVisible )
+    // }
+    visibleUsers: state => state.users.filter( user => user.isVisible ),
+
+    getUserById: state => id => state.users.find( user => user.id === id ),
+
   },
   mutations: {
+    increment(state) {
+      state.count++;
+    },
+    addCount( state, payload ) { // 第二引数はオブジェクト
+      state.count += payload.value;
+    }
   },
   actions: {
+    incrementAction( { commit }){
+      commit('increment');
+    },
+    addCountAction( { commit }, payload ) {
+      commit('addCount', payload )
+    }
   },
   modules: {
   }
